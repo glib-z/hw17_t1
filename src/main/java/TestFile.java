095483d74326dca1e77a1088e6c6b3befcf9749c
Glib Zamikula <zamikula.glib@gmail.com>
2019-03-24 22:30:25 +0200

    Initial commit

---
+++ src/main/java/TestFile.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+public class TestFile {
+
+    public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+        File homeDir = new File(userDir);
+        if (homeDir.exists() && homeDir.isDirectory()) {
+            printFilesName(homeDir.getAbsolutePath());
+        }
+    }
+
+    private static void printFilesName(String filename) {
+        File homeDir = new File(filename);
+        File[] files = homeDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                printFilesName(file.getAbsolutePath());
+            } else {
+                System.out.println(file.getName());
+            }
+        }
+
+    }
+
+}
